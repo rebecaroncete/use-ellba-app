2ff60ddc5f1d527f399d2c11e6829539dea15cf6
rebecaroncete <rebecaroncete@gmail.com>
2026-08-21 16:48:07 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // ======================================================================
 
 // Cole aqui o link "…/exec" da implantação do seu Apps Script:
-const API_URL = "COLE_AQUI_O_LINK_DO_SEU_APPS_SCRIPT";
+const API_URL = "https://script.google.com/macros/s/AKfycbz1YUga8xpmd-FOB3zcmKk1Es5YrnxgxjF3yes6bpL6J8T-sUP-_eE1LXYOJ3PbbqP3/exec";
 
 // Cole aqui A MESMA chave que você colocou em CHAVE_SECRETA no Code.gs:
-const CHAVE_API = "COLE_AQUI_A_MESMA_CHAVE_DO_CODE_GS";
+const CHAVE_API = "a0cuYB1XoyybHztQ9LMqVmC5ZESYJCuGemvtSqj-Esw";
